6a90e77e4b2f31050de6b3493889eca9c32b9e45
teodor <teodor@teodor-N53SV.(none)>
2012-11-12 07:15:11 +0100

    syncing machines

--- ragnarok/makedb.py
+++ ragnarok/makedb.py
@@ -2,80 +2,80 @@ import re
 import sqlite3
 import codecs
 
-# classes
-class item_model():
-	def __init__(self, ID, name, desc, buy=0, sell=0, ATK =0, DEF =0):
-		self.ID = ID
-		self.name = name
-		self.desc = desc
-		self.buy  = buy
-		self.sell = sell
-		self.ATK  = ATK
-		self.DEF  = DEF
-
-	def __str__(self):
-		return "{0} : {1} : {2} : {3} : {4} : {5} : {6}".format(self.ID, self.name, self.desc, self.buy, self.sell, self.ATK, self.DEF)
-
 # functions
-def right_item(ID, name, desc, buy=0, sell=0, ATK=0, DEF=0):
-	if ID == '':
-		ID = 0
-	if buy == '':
-		buy = 0
-	if sell == '':
-		sell = 0
-	if ATK == '':
-		ATK = 0
-	if DEF == '':
-		DEF = 0
-		
-	return (int(ID), name, desc, int(buy), int(sell), int(ATK), int(DEF))
-	
-# Variables
+def right_item(ID, name, desc, buy=0, sell=0, attack=0, defence=0):
+    # If we don't check we could get a cast error
+    if ID == '':
+        ID = 0
+    if buy == '':
+        buy = 0
+    if sell == '':
+        sell = 0
+    if attack == '':
+        attack = 0
+    if defence == '':
+        defence = 0
+
+    # We need to return the list with all the elements in right types.
+    return (int(ID), name, desc, int(buy), int(sell), int(attack), int(defence))
+
+# Variables =============
+
+# File variables
 txtdb = codecs.open('item_db.txt',encoding='utf-8')
 descdb = codecs.open('data/idnum2itemdesctable.txt', encoding='utf-8').read()
+
+# SQL Variables
 conn = sqlite3.connect('ro.db')
 c = conn.cursor()
 
+# Description variables
 descdb_re = re.compile(r"([0-9]+)#([^#]+)", re.MULTILINE)
 desc_clean_re = re.compile(r"(\^[0-9]+|\r|\t)")
 clean_desc = {}
 
+# The final list
 items = []
-items_num = 0
 
-# Actual script
+# Action part =======================
 
-# Parsing the descriptions
+# We need to
+#TODO FIX VARIABLES NAMES!
 raw_desc = descdb_re.findall(descdb)
 for group in raw_desc:
-	local_desc = desc_clean_re.sub('', group[1])
-	local_desc1 = local_desc.split('\n')
-	clean_desc['{0}'.format(group[0])] = local_desc1[1]
+    local_desc  = desc_clean_re.sub('', group[1])
+    local_desc1 = local_desc.split('\n')
+    clean_desc['{0}'.format(group[0])] = local_desc1[1]
 
 # The item_db.txt is in a handy 1-line-1-item format, so we are doing one line at a time
 for line in txtdb:
-	if re.match(r"[0-9]+,[^,]+", line):
-		info = line.split(',')
+    if re.match(r"[0-9]+,[^,]+", line):
+        # Since the files have such a nice cvs like structure this is enough.
+        info = line.split(',')
 
-		if info[0] not in clean_desc:
-			local_desc = "No description found."
-		else:
-			local_desc = clean_desc[info[0]]
-		
-		ID = info[0]
-		name = info[2]
-		buy = info[4]
-		sell = info[5]
-		atk = info[7]
-		defence = info[8]
-		#				ID       NAME       DESC      BUY      SELL     ATK      DEF
-		items.append(right_item(ID, name, local_desc, buy, sell, atk, defence))
-		items_num += 1
+        # We can't be sure that all items are going to have a description.
+        if info[0] not in clean_desc:
+            local_desc = "No description found."
+        else:
+            local_desc = clean_desc[info[0]]
 
+        # Declaring them with a normal name makes it much more readable.
+        ID      = info[0]
+        name    = info[2]
+        buy     = info[4]
+        sell    = info[5]
+        attack  = info[7]
+        defence = info[8]
 
-# THE SQL PART.
+        # We are creating a list so that we can iterate it later.
+        items.append(right_item(ID, name, local_desc, buy, sell, attack, defence))
+
+# THE SQL PART ========================
+
+# Checking if the table is allready there. If it is we ain't touching it.
 c.execute('DROP TABLE IF EXISTS items')
+
+# We need a table to work in.
 c.execute('''CREATE TABLE items (
     idkey       INTEGER PRIMARY KEY,
     ID          INTEGER,
@@ -83,16 +83,18 @@ c.execute('''CREATE TABLE items (
     description TEXT,
     buy         INTEGER,
     sell        INTEGER,
-    atk         INTEGER,
-    def         INTEGER
+    attack      INTEGER,
+    defence     INTEGER
 );''')
 
-
-
-# Just for developing reasons
+# This is why we stored the items in a list. Now we can just insert all of them in one line.
 c.executemany('INSERT INTO items VALUES (NULL, ?, ?, ?, ?, ?, ?, ?)', items)
 
-conn.commit()
-# Cleaning up
+# Cleaning up =====
+
+# FILE
 txtdb.close()
+
+# SQL
+conn.commit()
 conn.close()
